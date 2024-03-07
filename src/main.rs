2ab889d8f887b63e23829acd709679fc98167777
murdex5 <fkadupitige@gmail.com>
2024-03-07 18:52:22 +0100

    added commanline arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::{env, process::exit};
+
 fn main() {
-    println!("Hello, world!");
+    // Handles user input
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("Usage: ./rpython <filename.rpy>");
+        exit(0);
+    }
+    
+    let last_four: String = args[1].chars().rev().take(4).collect::<String>().chars().rev().collect();
+    if last_four != ".rpy" {
+        println!("filetype should be .rpy");
+        exit(0);
+    }
+
 }
